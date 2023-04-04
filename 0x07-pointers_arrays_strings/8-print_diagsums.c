33981fde7fb704c1dd3b606791af5e89c395b2dc
Amr Elstohy <125589444+amrelstohy@users.noreply.github.com>
2023-04-04 03:45:34 +0200

    Add files via upload

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,37 @@
-#include "holberton.h"
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 8-print_diagsums.c                           /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: Barahmou   <hamabarhamou@gmail.com>    /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/* Created: 2022-03-28 09:44:03   $Barahmou           \/        \/      \_/  */
+/* Updated: 2022-03-28 09:44:03 by Barahmou                                  */
+/*                                                                           */
+/*****************************************************************************/
+
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sum of the two diagnols of square matrix.
+ * print_diagsums - a function ...
+ * @a: the matrix
+ * @size: the length of matrice
  *
- * @a: pointer to array element.
- * @size: size of matrix.
- * Return: nothing.
+ * Return: 1 or 0
  */
+
+
 void print_diagsums(int *a, int size)
 {
-	int m, sum, sum1;
-
-	sum = 0;
-
-	for (m = 0; m < size; m++)
-		sum += *(a + ((size * m) + m));
-	sum1 = 0;
-	for (m = 1; m <= size; m++)
-		sum1 += *(a + ((size * m) - m));
-	printf("%d, %d\n", sum, sum1);
+	int	i =0, *ptr, j = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		ptr = a;
+		for (j = 0 ; j < size; j++)
+			printf("%d ",ptr[j]);
+		ptr++;
+		printf("\n");
+	}
 }
+
