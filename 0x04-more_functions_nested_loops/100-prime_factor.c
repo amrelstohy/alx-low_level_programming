a0e43cb8280a9e917357f9a9d0865c9ffba5d219
amrelstohy <amramr2901@gmail.com>
2023-04-14 19:46:38 -0700

    f

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, m, x;
+	long int n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			m = i;
+			i = 1;
+		}
+	}
+	printf("%d\n", m);
+	return (0);
+}
