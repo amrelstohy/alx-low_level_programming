8fa6da6c11631e77e7f94b6016966b4a92bc7adb
amr elstohy <amramr2901@gmail.com>
2023-05-05 00:01:33 +0000

    hi

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - function
+ *
+ * @b: variable
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int x, i = 0, z = 0, d = 0, c = 1;
+
+	while (b[i])
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		i++;
+	}
+	for (x = 0; x < i; x++)
+	{
+		z = z * 10;
+		z = z + (b[x] - 48);
+	}
+	for (x = 0; x <= i; x++)
+	{
+		d = d + ((z % 10) * c);
+		z = z / 10;
+		c = c * 2;
+	}
+	return (d);
+}
