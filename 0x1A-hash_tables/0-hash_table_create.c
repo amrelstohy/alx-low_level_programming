2b17b2e542fd0ecbe721baf7ebd7338a40088a80
amr elstohy <amramr2901@gmail.com>
2023-07-07 15:51:16 +0000

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,9 +6,11 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = calloc(size, sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
 	return (table);
 }
