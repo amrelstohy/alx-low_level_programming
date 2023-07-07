0c2deceb3ba418b258b369021b201f51db86b8ed
amr elstohy <amramr2901@gmail.com>
2023-07-07 15:52:20 +0000

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,6 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t *));
+	table->array = calloc(size, sizeof(long));
 	return (table);
 }
